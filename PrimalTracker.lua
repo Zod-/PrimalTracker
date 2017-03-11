6a787a28ea710b207912ab92f127f63000500c9f
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-03-11 02:39:25 +0100

    Converted Aramunns addon into a standalone

--- PrimalTracker.lua
+++ PrimalTracker.lua
@@ -6,12 +6,145 @@ function PrimalTracker:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
-
+  self.isXMLLoaded = false
+  self.saveData = {
+    rewards = {}
+  }
   return o
 end
 
 function PrimalTracker:Init()
   Apollo.RegisterAddon(self, false, "")
+end
+
+function PrimalTracker:IsLoaded()
+  return self.isXMLLoaded
+end
+
+function PrimalTracker:OnLoad()
+  self:LoadMatchMaker()
+  self:BindHooks()
+  self:LoadXML()
+end
+
+function PrimalTracker:LoadXML()
+  self.xmlDoc = XmlDoc.CreateFromFile("PrimalTracker.xml")
+  self.xmlDoc:RegisterCallback("OnDocumentLoaded", self)
+end
+
+function PrimalTracker:OnDocumentLoaded(args)
+  self.isXMLLoaded = true
+end
+
+function PrimalTracker:LoadMatchMaker()
+  self.addonMatchMaker = Apollo.GetAddon("MatchMaker")
+end
+
+function PrimalTracker:BindHooks()
+  if not self.addonMatchMaker then return false end
+
+  local originalBuildFeaturedList = self.addonMatchMaker.BuildFeaturedList
+  self.addonMatchMaker.BuildFeaturedList = function(...)
+    originalBuildFeaturedList(...)
+    if self:IsLoaded() then
+      self:PlaceOverlays()
+    end
+  end
+end
+
+function PrimalTracker:PlaceOverlays()
+  local currentSecconds = self:GetCurrentSecconds()
+  local rewardWindows = self:GetRewardWindows()
+  for i = 1, #rewardWindows do
+    local rewardWindow = rewardWindows[i]
+    local rewardData = self:GetRewardData(rewardWindow, currentSecconds)
+    self:BuildOverlay(rewardWindow, rewardData)
+  end
+end
+
+function PrimalTracker:GetCurrentSecconds()
+  local currentTime = GameLib.GetServerTime()
+  return os.time({
+      ["year"] = currentTime.nYear,
+      ["month"] = currentTime.nMonth,
+      ["day"] = currentTime.nDay,
+      ["hour"] = currentTime.nHour,
+      ["min"] = currentTime.nMinute,
+      ["sec"] = currentTime.nSecond,
+    }
+  )
+end
+
+function PrimalTracker:GetRewardWindows()
+  --self.addonMatchMaker.tWndRefs.wndMain:FindChild("TabContent:RewardContent"):GetChildren()
+  local rewards = self.addonMatchMaker
+  rewards = rewards and rewards.tWndRefs
+  rewards = rewards and rewards.wndMain
+  rewards = rewards and rewards:FindChild("TabContent:RewardContent")
+  rewards = rewards and rewards:GetChildren() or {}
+  return rewards
+end
+
+function PrimalTracker:BuildOverlay(rewardWindow, rewardData)
+  local overlay = Apollo.LoadForm(self.xmlDoc, "Overlay", rewardWindow, self)
+  overlay:FindChild("Completed"):SetData(rewardData)
+  if self:IsRewardActive(rewardData) then
+    overlay:FindChild("Shader"):Show(false)
+  else
+    overlay:FindChild("Completed"):SetCheck(true)
+  end
+end
+
+function PrimalTracker:IsRewardActive(rewardData)
+  for i = 1, #self.saveData.rewards do
+    if self:IsSameReward(rewardData, self.saveData.rewards[i]) then
+      return false
+    end
+  end
+  return true
+end
+
+function PrimalTracker:IsSameReward(tA, tB)
+  if tA.multiplier ~= tB.multiplier then return false end
+  local endDiff = math.abs(tA.endTime - tB.endTime)
+  if endDiff > 60 then return false end
+  if tA.contentName ~= tB.contentName then return false end
+  return true
+end
+
+function PrimalTracker:OnCompletedCheck(handler, control)
+  table.insert(self.saveData.rewards, control:GetData())
+  control:GetParent():FindChild("Shader"):Show(true)
+end
+
+function PrimalTracker:OnCompletedUncheck(handler, control)
+  local rewardData = control:GetData()
+  for i = 1, #self.saveData.rewards do
+    if self:IsSameReward(rewardData, self.saveData.rewards[i]) then
+      table.remove(self.saveData.rewards, i)
+      break
+    end
+  end
+  control:GetParent():FindChild("Shader"):Show(false)
+end
+
+function PrimalTracker:GetRewardData(rewardWindow, currentSecconds)
+  local rawData = rewardWindow:FindChild("InfoButton"):GetData()
+  return {
+    contentName = rawData.strContentName,
+    endTime = currentSecconds + rawData.nSecondsRemaining,
+    multiplier = rawData.tRewardInfo.nMultiplier,
+  }
+end
+
+function PrimalTracker:OnSave(saveLevel)
+  if saveLevel ~= GameLib.CodeEnumAddonSaveLevel.Realm then return end
+  return self.saveData
+end
+
+function PrimalTracker:OnRestore(saveLevel, saveData)
+  if saveLevel ~= GameLib.CodeEnumAddonSaveLevel.Realm then return end
+  self.saveData = saveData
 end
 
 local PrimalTrackerInst = PrimalTracker:new()
